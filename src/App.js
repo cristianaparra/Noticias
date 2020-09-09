93a5056a9950718c4b56263bbe425484e39c91fe
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-08 21:11:00 -0300

    Creando stylos en formulario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './componnents/Header'
+import Formulario from './componnents/Formulario'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         titulo='Buscador de Noticias'
       />
       <div className='container white'>
-        <h1>Formulario</h1>
+        <Formulario />
       </div>
     </Fragment>
   );
