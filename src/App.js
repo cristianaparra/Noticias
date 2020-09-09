cba668a3ca83b6ca7d03da4ba10887d0898df3ad
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-08 22:28:54 -0300

    consulta api de noticias y creacion de state noticias

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './componnents/Header'
 import Formulario from './componnents/Formulario'
 
@@ -6,7 +6,20 @@ function App() {
 
   // definir categoria
 
-  const [categoria, guardarCategoria] = useState('')
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+
+    const consultarAPI = async () => {
+      const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b7bd8c20f52f44a2beccc2af97675cb8'
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      guardarNoticias(noticias.articles)
+    }
+    consultarAPI()
+  }, [categoria])
+
   return (
 
     <Fragment>
@@ -14,8 +27,8 @@ function App() {
         titulo='Buscador de Noticias'
       />
       <div className='container white'>
-        <Formulario 
-        guardarCategoria={guardarCategoria}/>
+        <Formulario
+          guardarCategoria={guardarCategoria} />
       </div>
     </Fragment>
   );
